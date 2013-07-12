92a5487ad7a06e2dd8c803b1ada2c42d505f95f0
Gabe <gabedominguez@outlook.com>
2013-07-12 09:35:56 -0700

    working on before filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
+    def require_login
+      unless logged_in?
+        flash[:error] = "You must be logged in to view this page"
+        redirect_to new_login_url
+      end
+    end
 
     def current_user
       if session[:user_id]
